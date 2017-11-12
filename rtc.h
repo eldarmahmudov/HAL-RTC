6f85906af06a0285e9bd934618297f913586a275
Nikolay Prizhimin <prinv@yandex.ru>
2017-11-12 12:26:06 +0500

    Update rtc.h

--- rtc.h
+++ rtc.h
@@ -6,8 +6,6 @@
 #define RTC_ADDR		(0x68<<1)
 
 int8_t DS1307Read(I2C_HandleTypeDef hi, uint8_t address, uint8_t *data);	// The function of reading data from DS1307
-
-
 int8_t DS1307Write(I2C_HandleTypeDef hi, uint8_t address, uint8_t data);	// The function of writing data to DS1307
 
 #endif
