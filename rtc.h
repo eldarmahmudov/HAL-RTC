1e18084092d89cfe8256a1f0f60e2a9f081271fa
Nikolay Prizhimin <prinv@yandex.ru>
2017-11-12 13:03:09 +0500

    Update rtc.h

--- rtc.h
+++ rtc.h
@@ -1,11 +1,14 @@
 #ifndef _DS1307_H
 #define _DS1307_H
 
+// #define	RTC_DS1307						// Uncomment this line for DS3107
+#define		RTC_DS3231						// Comment this line for DS3107
+
 #include "stm32f1xx_hal.h"
 
 #define RTC_ADDR		(0x68<<1)
-
-int8_t DS1307Read(I2C_HandleTypeDef hi, uint8_t address, uint8_t *data);	// The function of reading data from DS1307
-int8_t DS1307Write(I2C_HandleTypeDef hi, uint8_t address, uint8_t data);	// The function of writing data to DS1307
+int8_t RTC_Init(I2C_HandleTypeDef hi);					// The function of initialization RTC
+int8_t RTC_Read(I2C_HandleTypeDef hi, uint8_t address, uint8_t *data);	// The function of reading data from RTC
+int8_t RTC_Write(I2C_HandleTypeDef hi, uint8_t address, uint8_t data);	// The function of writing data to RTC
 
 #endif
